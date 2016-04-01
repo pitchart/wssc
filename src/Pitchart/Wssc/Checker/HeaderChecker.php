2aa7d77a6838f36bb2b99bb4944d7c5c72799bcf
Julien VITTE <vitte.julien@gmail.com>
2016-04-01 11:12:36 +0200

    [FEATURE] generic check for chekers based on HTTP headers

---
+++ src/Pitchart/Wssc/Checker/HeaderChecker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pitchart\Wssc\Checker;
+
+use Pitchart\Wssc\Http\Response;
+
+abstract class HeaderChecker implements Checker {
+
+    /**
+     * The header to check
+     * @var string
+     */
+    protected $headerName;
+
+    /**
+     * Pattern validating the test
+     * @var string
+     */
+    protected $matchingPattern = '';
+
+    /**
+     * @inheritedDoc
+     */
+    public function check(Response $response) {
+        return $response->hasHeader($this->headerName)
+            && preg_match($this->matchingPattern, $response->getHeader($this->headerName)->getValue());
+    }
+}
