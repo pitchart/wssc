66bd1aaf5ee552a8ceb05a50dc50cf92c4c099f1
Julien VITTE <vitte.julien@gmail.com>
2016-04-07 23:11:18 +0200

    [FEATURE] add tests for status checks

---
+++ src/Pitchart/Wssc/Tests/Checker/HttpStatusCheckerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pitchart\Wssc\Tests\Checker;
+
+use Pitchart\Wssc\Checker\Checker;
+use Pitchart\Wssc\Checker\HttpStatusChecker;
+use Pitchart\Wssc\Http\Response;
+
+
+class HttpStatusCheckerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var HttpStatusChecker
+     */
+    protected $checker;
+
+    public function setUp()
+    {
+        $this->checker = new HttpStatusChecker();
+    }
+
+    public function testCanBeInstanciated()
+    {
+        $this->assertInstanceOf(Checker::class, $this->checker);
+        $this->assertInstanceOf(HttpStatusChecker::class, $this->checker);
+    }
+
+    public function testMustReturnTrueFor301Status() {
+        $response = Response::fromPlainText('HTTP/1.0 301 MOVED PERMANENTLY');
+        $this->assertTrue($this->checker->check($response));
+    }
+
+    public function testMustReturnFalseForNon301Status() {
+        $response = Response::fromPlainText('HTTP/1.0 200 OK');
+        $this->assertFalse($this->checker->check($response));
+    }
+
+}
---
+++ src/Pitchart/Wssc/Tests/Checker/HttpsStatusCheckerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pitchart\Wssc\Tests\Checker;
+
+use Pitchart\Wssc\Checker\Checker;
+use Pitchart\Wssc\Checker\HttpsStatusChecker;
+use Pitchart\Wssc\Http\Response;
+
+
+class HttpsStatusCheckerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var HttpsStatusChecker
+     */
+    protected $checker;
+
+    public function setUp()
+    {
+        $this->checker = new HttpsStatusChecker();
+    }
+
+    public function testCanBeInstanciated()
+    {
+        $this->assertInstanceOf(Checker::class, $this->checker);
+        $this->assertInstanceOf(HttpsStatusChecker::class, $this->checker);
+    }
+
+    public function testMustReturnTrueFor200Status() {
+        $response = Response::fromPlainText('HTTP/1.0 200 OK');
+        $this->assertTrue($this->checker->check($response));
+    }
+
+    public function testMustReturnFalseForNon301Status() {
+        $response = Response::fromPlainText('HTTP/1.0 404 NOT FOUND');
+        $this->assertFalse($this->checker->check($response));
+    }
+
+}
